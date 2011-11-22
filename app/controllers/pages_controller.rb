cdd008bf441506acc15af74238a65ab263d4bb6a
matthew gamble <matt_gamble07@hotmail.com>
2011-11-22 12:11:18 -0600

    production update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
-class UsersController < ApplicationController
-def home
+class PagesController < ApplicationController
+  def home
     @title = "Home"
   end
 
   def contact
     @title = "Contact"
   end
+  
+  def about
+    @title = "About"
+  end
+  
+  def help
+    @title = "Help"
+  end
+
 end
