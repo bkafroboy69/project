dbe5e81a79850be4b3c1e17e43b742b839210169
matthew gamble <matt_gamble07@hotmail.com>
2011-11-22 11:42:43 -0600

    production update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
-class PagesController < ApplicationController
-  def home
+class UsersController < ApplicationController
+  def new
+    @title = "Sign up"
+    @user = User.new
   end
-
-  def contact
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
-
+    def show
+      @user = User.find(params[:id])
+      @title = @user.name
+    end
 end
