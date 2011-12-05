99dd0f2dfa9657d3aa93331f05c2438d1457581f
matthew gamble <matt_gamble07@hotmail.com>
2011-12-05 09:49:42 -0600

    sign up update

--- app/controllers/micropost_controller.rb
+++ app/controllers/micropost_controller.rb
@@ -1,9 +1,16 @@
-class MicropostsController < ActionController::Base
+class MicropostsController < ApplicationController
+ 
   def create
-    
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
   end
-  
-  def destory
-    
+
+  def destroy
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 SampleApp4::Application.routes.draw do
  
-
-
-
     resources :users
     resources :sessions, :only => [:new, :create, :destroy]
-    
+    resources :microposts, :only => [:create, :destroy]
  
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
