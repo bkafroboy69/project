8aed6c806c578567fa10c9ea66f1c0efcfe75a5f
matthew gamble <matt_gamble07@hotmail.com>
2011-12-08 12:43:43 -0600

    profiles updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #sign_in user
+      sign_in user
       redirect_to user
     end
   end
