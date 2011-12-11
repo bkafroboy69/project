16249693057e8e04c83346348f3c1fe16d7141be
matthew gamble <matt_gamble07@hotmail.com>
2011-12-11 14:45:08 -0600

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp4::Application.routes.draw do
  
     resources :users
+    resources :microposts, :only =>[:create, :destroy]
     resources :sessions, :only => [:new, :create, :destroy]
  
  
